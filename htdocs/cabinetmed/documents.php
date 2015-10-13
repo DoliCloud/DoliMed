d869ae3a1262c7404ef5a6890eb6446c2838317a
Laurent Destailleur <eldy@destailleur.fr>
2015-10-13 11:22:12 +0200

    GETPOST au lieu de POST

--- htdocs/cabinetmed/documents.php
+++ htdocs/cabinetmed/documents.php
@@ -171,7 +171,7 @@ if (! empty($_POST['removedfile']))
     $upload_dir_tmp = $vardir.'/temp';
 
 	// TODO Delete only files that was uploaded from email form
-    $mesg=dol_remove_file_process($_POST['removedfile'],0);
+    $mesg=dol_remove_file_process(GETPOST('removedfile','alpha'),0);
 
     $action='presend';
     $_POST["action"]='presend';
