4e0fa089fe3178673570fa04188234dc925af2f0
Laurent Destailleur <eldy@destailleur.fr>
2015-11-15 19:29:47 +0100

    Sort module into HR section for 3.9

--- htdocs/cabinetmed/core/modules/modCabinetMed.class.php
+++ htdocs/cabinetmed/core/modules/modCabinetMed.class.php
@@ -53,13 +53,13 @@ class modCabinetMed extends DolibarrModules
 
         // Family can be 'crm','financial','hr','projects','products','ecm','technic','other'
         // It is used to group modules in module setup page
-        $this->family = "other";
+        $this->family = "hr";
         // Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
         $this->name = preg_replace('/^mod/i','',get_class($this));
         // Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
         $this->description = "Module CabinetMed";
         // Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-        $this->version = '3.7.0';
+        $this->version = '3.9.0';
         // Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
         $this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
         // Where to store the module in setup page (0=common,1=interface,2=others,3=very specific)
