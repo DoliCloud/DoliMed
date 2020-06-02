c07b4e7aec2dd8130fb0b10862b9a1419f1b718f
Laurent Destailleur <eldy@destailleur.fr>
2020-06-02 09:19:04 +0200

    Fix free resql

--- htdocs/cabinetmed/listconsult.php
+++ htdocs/cabinetmed/listconsult.php
@@ -669,7 +669,7 @@ if ($resql)
 	$reshook=$hookmanager->executeHooks('printFieldListFooter',$parameters);    // Note that $action and $object may have been modified by hook
 	print $hookmanager->resPrint;
 
-	$db->free($result);
+	$db->free($resql);
 }
 else
 {
