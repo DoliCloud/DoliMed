a86b0b908a29a20b2153b4326219f982b3395cb6
Laurent Destailleur <eldy@destailleur.fr>
2023-12-04 11:54:31 +0100

    Fix phpcs

--- dev/translation/autotranslator.class.php
+++ dev/translation/autotranslator.class.php
@@ -64,7 +64,7 @@ class autoTranslator
 		// Translate
 		//ini_set('default_charset','UTF-8');
 		ini_set('default_charset',$this->_outputpagecode);
-		$this->parse_refLangTranslationFiles();
+		$this->parseRefLangTranslationFiles();
 
 	}
 
@@ -73,7 +73,7 @@ class autoTranslator
 	 *
 	 * 	@return	void
 	 */
-	private function parse_refLangTranslationFiles()
+	private function parseRefLangTranslationFiles()
 	{
 
 		$files = $this->getTranslationFilesArray($this->_refLang);
