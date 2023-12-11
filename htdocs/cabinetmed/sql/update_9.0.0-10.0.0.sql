e43968b0272c47037aeb17cbda06921e2f8ca4c0
Laurent Destailleur <eldy@destailleur.fr>
2023-12-11 10:14:05 +0100

    Fix migration

--- htdocs/cabinetmed/sql/update_9.0.0-10.0.0.sql
+++ htdocs/cabinetmed/sql/update_9.0.0-10.0.0.sql
@@ -2,11 +2,12 @@
 -- Script run to make a migration of module version x.x.x to module version y.y.y
 --
  
- 
+
 ALTER TABLE llx_cabinetmed_cons ADD COLUMN entity integer DEFAULT 1 NOT NULL;
 
 ALTER TABLE llx_cabinetmed_motifcons ADD COLUMN lang varchar(12) NULL;
 
 ALTER TABLE llx_cabinetmed_examenprescrit ADD COLUMN lang varchar(12) NULL;
+
 ALTER TABLE llx_cabinetmed_c_examconclusion ADD COLUMN lang varchar(12) NULL;
 
