91ead25a2ab6f3078c15eee6fe03269a7c3c8990
Laurent Destailleur <eldy@users.sourceforge.net>
2024-04-30 00:30:35 +0200

    Clean code

--- htdocs/cabinetmed/class/patient.class.php
+++ htdocs/cabinetmed/class/patient.class.php
@@ -520,7 +520,6 @@ class Patient extends Societe
 				$this->note_private = $obj->note_private;
 				$this->note_public = $obj->note_public;
 				$this->model_pdf = $obj->model_pdf;
-				$this->modelpdf = $obj->model_pdf;
 				$this->default_lang = $obj->default_lang;
 				$this->logo = $obj->logo;
 
